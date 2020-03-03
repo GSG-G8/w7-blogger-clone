9199bbc8c35fcac2fceb0463c392dac917864fa5
mFlaifel <msf-2oo7@hotmail.com>
2020-03-03 15:08:09 +0200

    update layout relates #6

---
+++ public/script/dom.js
@@ -0,0 +1,35 @@
+const postFormBtn = document.querySelector('.show-create-post');
+const loginFormBtn = document.querySelector('.show-login-form');
+const signupFormBtn = document.querySelector('.show-signup-form');
+const cancelBtn = document.querySelectorAll('.form-exit');
+const loginForm = document.querySelector('.login-form');
+const signupForm = document.querySelector('.signup-form');
+const createPostForm = document.querySelector('.create-post-form');
+
+postFormBtn.addEventListener('click', () => {
+  loginForm.style.display = 'none';
+  signupForm.style.display = 'none';
+  createPostForm.style.display = 'block';
+});
+
+
+loginFormBtn.addEventListener('click', () => {
+  loginForm.style.display = 'block';
+  createPostForm.style.display = 'none';
+  signupForm.style.display = 'none';
+});
+
+
+signupFormBtn.addEventListener('click', () => {
+  signupForm.style.display = 'block';
+  loginForm.style.display = 'none';
+  createPostForm.style.display = 'none';
+});
+
+cancelBtn.forEach((e) => {
+  e.addEventListener('click', (e) => {
+    loginForm.style.display = 'none';
+    createPostForm.style.display = 'none';
+    signupForm.style.display = 'none';
+  });
+});
